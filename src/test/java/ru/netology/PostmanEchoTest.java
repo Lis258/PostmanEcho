c483db9b444739f16e78bcc490ca590cce408f36
Lis258 <avb-90@ya.ru>
2021-03-16 19:47:22 +0300

    PostmanEchoTest.java corrected

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,6 +15,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Some data"));
+                .body("data", equalTo("Some info"));
     }
 }
